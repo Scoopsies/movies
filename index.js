db7cf0415bbee2540b9c6a4af9b6070ab0a4d475
Scoopsies <mooninites103@gmail.com>
2023-09-21 19:36:36 -0500

    got movies to show up on webpage

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const client = new pg.Client('postgres://localhost/movies_db');
 const express = require('express');
 const app = express();
 const path = require('path');
+const { cli } = require('webpack');
 
 const homePage = path.join(__dirname, 'index.html');
 app.get('/', (req, res)=> res.sendFile(homePage));
@@ -16,14 +17,49 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
+app.get('/api/movies', async(req,res,next) =>{
+  try {
+    const SQL=`
+      SELECT *
+      FROM movies
+    `
+    const response = await client.query(SQL)
+    res.send(response.rows)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.get('/api/movies/:id', async(req,res,next) => {
+  try {
+    const SQL =`
+      SELECT *
+      FROM MOVIES
+      WHERE id=$1
+    `;
+    const response = await client.query(SQL, [req.params.id]);
+    res.send(response.rows[0])
+  } catch (error) {
+    next(error)
+  }
+})
+
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
+    DROP TABLE IF EXISTS movies;
+    CREATE TABLE movies(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(50),
+      stars INT
+    );
+    INSERT INTO movies(name, stars) VALUES('The Beyond', 5);
+    INSERT INTO movies(name, stars) VALUES('The Descent', 4);
+    INSERT INTO movies(name, stars) VALUES('The Conjuring', 5);
   `;
-  console.log('create your tables and seed data');
-
+  await client.query(SQL);
+  console.log('Table created and Seeded')
   const port = process.env.PORT || 3000;
   app.listen(port, ()=> {
     console.log(`listening on port ${port}`);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+
 
 const App = ()=> {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getData = async() => {
+      const {data} = await axios.get('/api/movies')
+      setMovies(data)
+    }
+    getData()
+  },[])
+
+  console.log(movies)
+
+
   return (
-    <h1>FullStack Template</h1>
+    <>
+      <h1>Movies ({movies.length})</h1>
+      <ul>
+        {
+          movies.map(movie => {
+            return <li key={movie.id}>{movie.name}</li>
+          })
+        }
+      </ul>
+    </>
   );
 };
 
